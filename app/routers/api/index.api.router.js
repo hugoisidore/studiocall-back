8315391140b0e937b9d6136e0cf2d1794aaa4a8b
SamuelJuminer <164910301+SamuelJuminer@users.noreply.github.com>
2024-10-23 16:56:09 +0200

    becomingpartnerrouter

--- app/routers/api/index.api.router.js
+++ app/routers/api/index.api.router.js
@@ -3,6 +3,7 @@ import messagesRouter from "./messages.router.js";
 import musicsRouter from "./musics.router.js";
 import sacemRouter from "./sacem.router.js";
 import aboutRouter from "./about.router.js";
+import partnersRouter from "./partners.router.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/messages", messagesRouter);
 router.use("/sacem", sacemRouter);
 router.use("/about", aboutRouter);
 router.use("/musics", musicsRouter);
+router.use("/becoming", partnersRouter);
 
 export default router;
