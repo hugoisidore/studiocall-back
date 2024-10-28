57a44f339a178e88c50112add4db57ddaca01022
hugoisidore <hugo.isidore@oclock.school>
2024-10-28 11:52:43 +0100

    Adding of advisor call router

--- app/routers/api/index.api.router.js
+++ app/routers/api/index.api.router.js
@@ -4,6 +4,7 @@ import musicsRouter from "./musics.router.js";
 import sacemRouter from "./sacem.router.js";
 import aboutRouter from "./about.router.js";
 import partnersRouter from "./partners.router.js";
+import advisorRouter from "./advisor.router.js";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use("/sacem", sacemRouter);
 router.use("/about", aboutRouter);
 router.use("/musics", musicsRouter);
 router.use("/becoming", partnersRouter);
+router.use("/advisor", advisorRouter);
 
 export default router;
