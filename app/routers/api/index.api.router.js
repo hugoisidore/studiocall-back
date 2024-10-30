633fd5c619f9dc629b1b278f32f72473a39a0c65
hugoisidore <hugo.isidore@oclock.school>
2024-10-30 12:11:59 +0100

    MAJ routes

--- app/routers/api/index.api.router.js
+++ app/routers/api/index.api.router.js
@@ -5,7 +5,6 @@ import sacemRouter from "./sacem.router.js";
 import aboutRouter from "./about.router.js";
 import partnersRouter from "./partners.router.js";
 import advisorRouter from "./advisor.router.js";
-import footerRouter from "./footer.router.js";
 
 const router = express.Router();
 
@@ -18,11 +17,5 @@ router.use("/advisor", advisorRouter);
 
 // body
 router.use("/musics", musicsRouter);
-
-// footer
-router.use("/legal", footerRouter);
-router.use("/rgpd", footerRouter);
-router.use("/site", footerRouter);
-router.use("/contact", footerRouter);
 
 export default router;
--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -2,8 +2,15 @@ import express from "express";
 import homeRouter from "./api/home.router.js";
 import apiRouter from "./api/index.api.router.js";
 import errorRouter from "./api/error.router.js";
+import footerRouter from "./api/footer.router.js";
 
 const router = express.Router();
+
+// footer
+router.use("/legal", footerRouter);
+router.use("/rgpd", footerRouter);
+router.use("/site", footerRouter);
+router.use("/contact", footerRouter);
 
 router.use("/", homeRouter);
 router.use("/api", apiRouter);
