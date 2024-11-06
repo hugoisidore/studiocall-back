98dd2b81311726bde0a90f21a7d460e53844a15e
hugoisidore <hugo.isidore@oclock.school>
2024-11-06 14:40:05 +0100

    seeding with voices and musics

--- app/controllers/messagesController.js
+++ app/controllers/messagesController.js
@@ -1,4 +1,5 @@
 import { Music } from '../models/Music.js';
+import { Voice } from '../models/Voice.js';
 import ApiError from '../errors/ApiError.js';
 
 const messagesController = {
@@ -22,11 +23,27 @@ const messagesController = {
       console.log("Categories found:", categories);
       console.log("Musics found:", musics);
 
+      // Récupération des catégories de voix
+    const voiceCategories = await Voice.findAll({
+      attributes: ['voice_category'],
+      group: ['voice_category'],
+    });
+
+    // Si aucune catégorie de voix n'est trouvée
+    if (voiceCategories.length === 0) {
+      return res.status(404).json({ message: 'Aucune catégorie de voix trouvée.' });
+    }
+
+    // Récupération de toutes les voix
+    const allVoices = await Voice.findAll({
+      attributes: ['voice_category', 'voice_title', 'file_voice'],
+    });
+
       // Envoie les résultats au front-end
-      res.render('createStandard', { categories, musics }); // Passez également `musics`
+      res.render('createStandard', { categories, musics, voiceCategories, allVoices }); 
     } catch (error) {
-      console.error("Erreur lors de la récupération des catégories:", error);
-      return next(new ApiError(500, "Erreur lors de la récupération des catégories"));
+      console.error("Erreur lors de la récupération des données:", error);
+      return next(new ApiError(500, "Erreur lors de la récupération des données"));
     }
   },
   async getCreateSmartphoneMessage(req, res, next) {
@@ -53,7 +70,7 @@ const messagesController = {
       console.error("Erreur lors du rendu de la vue examplesMessages:", error);
       return next(new ApiError(500, "Erreur lors du rendu de la vue Examples of Messages"));
     }
-  }
+  },
 };
 
 export default messagesController;
